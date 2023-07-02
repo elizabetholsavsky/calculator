0a84c9d36b522ca6d565d01b6c6ef0dc97a0c060
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-07-01 21:50:24 -0500

    delete btn working, will return null instead of empty string if all digits deleted

--- src/App.js
+++ src/App.js
@@ -14,6 +14,12 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
@@ -54,6 +60,37 @@ function reducer(state, { type, payload }) {
         }
       case ACTIONS.CLEAR:
         return {}
+      case ACTIONS.DELETE_DIGIT:
+        if (state.overwrite) {
+          return {
+            ...state,
+            overwrite: false,
+            currentOperand: null
+          }
+        }
+        if (state.currentOperand == null) return state
+        if (state.currentOperand.length === 1) {
+          return { ...state, currentOperand: null }
+        }
+
+        return {
+          ...state,
+          currentOperand: state.currentOperand.slice(0, -1)
+        }
+      case ACTIONS.EVALUATE:
+        if (state.operation == null || 
+          state.currentOperand == null || 
+          state.previousOperand == null) {
+          return state
+        }
+
+        return {
+          ...state,
+          previousOperand: null,
+          overwrite: true,
+          operation: null,
+          currentOperand: evaluate(state),
+        }
   }
 }
 
@@ -92,8 +129,9 @@ function App() {
           <div className= "previous-operand">{previousOperand} {operation}</div>
           <div className= "current-operand">{currentOperand}</div>
       </div> 
-      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>
+        AC</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -109,8 +147,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE })}>=</button>
     </div> 
   )
 } 
